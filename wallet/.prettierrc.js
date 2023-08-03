2dada945fb67f2289a10e1375934c13d70764f89
YoanaGerling <yoanagerling@gmail.com>
2023-08-03 16:45:39 -0300

    Registro de transacciones. Request POST

---
+++ wallet/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "printWidth": 100,
+    "tabWidth": 2,
+    "singleQuote": true,
+    "trailingComma": "es5",
+    "endOfLine": "crlf", //Configuración para normalizar los caracteres de nueva línea
+    "endOfLine": "auto"
+}
+
+  
--- wallet/src/views/RegistroVista.vue
+++ wallet/src/views/RegistroVista.vue
@@ -1,52 +1,116 @@
 <template>
   <div class="row justify-content-center">
     <div class="col-4">
-      <form>
-        <h5 class="text-muted">
-          Formulario para el registro de una transacción
-        </h5>
+      <form @submit.prevent>
+        <!--Prevent: modificador que previene el comportamiento por defecto del nav de refrescar-->
+        <h5 class="text-muted">Formulario para el registro de una transacción</h5>
         <br />
-        <select class="form-select" aria-label="Default select example">
-          <option selected>Selecciona el tipo de transacción</option>
-          <option value="Compra">Compra</option>
-          <option value="Venta">Venta</option>
+        <label class="mb-1">Selecciona el tipo de transaccion</label>
+        <select
+          class="form-select"
+          aria-label="Default select example"
+          id="transaccion"
+          v-model="transaccion"
+        >
+          <option value="purchase">Compra</option>
+          <option value="sale">Venta</option>
         </select>
         <br />
-        <select class="form-select" aria-label="Default select example">
-          <option selected>Selecciona la criptomoneda</option>
-          <option value="BITCOIN">BITCOIN</option>
-          <option value="ETHEREUM">ETHEREUM</option>
-          <option value="USDT">USDT</option>
-          <option value="ADA">ADA</option>
+        <label class="mb-1">Selecciona la criptomoneda</label>
+        <select
+          class="form-select"
+          aria-label="Default select example"
+          id="cripto"
+          v-model="cripto"
+        >
+          <option value="btc">BITCOIN</option>
+          <option value="eth">ETHEREUM</option>
+          <option value="usdt">USDT</option>
+          <option value="ada">ADA</option>
         </select>
         <br />
-        <label>Cantidad</label>
-        <input type="text" class="form-control" placeholder="0.00" />
+        <label class="mb-1">Cantidad</label>
+        <input type="text" class="form-control" id="cantidad" v-model="cantidad" />
         <br />
-        <label>Precio</label>
-        <input type="text" class="form-control" placeholder="0.00" />
+        <label class="mb-1">Precio</label>
+        <input type="text" class="form-control" id="precio" v-model="precio" />
         <br />
-        <label>Fecha</label>
+        <label class="mb-1">Fecha</label>
         <br />
-        <input type="date" name="fecha" class="form-control" />
+        <input type="date" name="fecha" class="form-control" id="fecha" v-model="fecha" />
         <br />
+        <label class="mb-1">Hora</label>
         <br />
-        <label>Hora</label>
+        <input type="time" name="hora" class="form-control" id="hora" v-model="hora" />
         <br />
-        <input type="time" name="hora" class="form-control" />
-        <br />
-        <br />
-        <button type="submit" class="btn btn-info">
-          Registrar transacción
-        </button>
+        <button type="submit" class="btn btn-info" @click="envioForm">Registrar transacción</button>
       </form>
     </div>
   </div>
 </template>
 
 <script>
+import Consultas from '@/servicios/Consultas.js';
 export default {
-  name: "RegistroVista",
+  name: 'RegistroVista',
+  data() {
+    return {
+      transaccion: null,
+      cripto: null,
+      cantidad: '',
+      precio: '',
+      fecha: null,
+      hora: null,
+    };
+  },
+  methods: {
+    envioForm() {
+      if (
+        this.transaccion === null ||
+        this.cripto === null ||
+        this.fecha === null ||
+        this.hora === null ||
+        this.cantidad === '' ||
+        this.precio === ''
+      ) {
+        alert('Debe completar todos los campos para poder registrar la operacion');
+        return;
+      }
+      if (this.cantidad < 0 || this.precio < 0) {
+        alert('Datos incorrectos');
+        return;
+      }
+      let fechaActual = new Date();
+      let fechaIngresada = new Date(this.fecha);
+
+      if (fechaIngresada > fechaActual) {
+        alert('Fecha incorrecta');
+        return;
+      }
+      Consultas.guardarTransacciones({
+        user_id: 'Yoana',
+        action: this.transaccion,
+        crypto_code: this.cripto,
+        crypto_amount: this.cantidad.toString(),
+        money: this.precio.toString(),
+        datetime: this.fecha + ' ' + this.hora,
+      })
+        .then((respuesta) => {
+          alert('Transaccion registrada con exito');
+          console.log(respuesta);
+          this.transaccion = null;
+          this.cripto = null;
+          this.cantidad = '';
+          this.precio = '';
+          this.fecha = null;
+          this.hora = null;
+        })
+        .catch((error) => {
+          alert('No se pudo registrar la transaccion');
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
 
